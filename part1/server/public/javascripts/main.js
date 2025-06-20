375ebeacd65d1fbf7b483aba6dfd34d654f7cbe3
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 01:51:29 +0000

    [automatic commit] 2025-06-20 01:51:29 UTC 1.158.136.101

--- part1/server/public/javascripts/main.js
+++ part1/server/public/javascripts/main.js
@@ -2,6 +2,8 @@ import { createApp, ref } from 'vue';
 
 createApp({
   setup() {
-    fetch("https://dog.ceo/api/breeds/image/random")
+    fetch("https://dog.ceo/api/breeds/image/random").then() {
+      
+    }
   }
 }).mount('#app');
