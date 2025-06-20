0e2b395f27aa7d7d398dbbe4b4880225378b7d9b
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 01:00:24 +0000

    [automatic commit] 2025-06-20 01:00:24 UTC 1.158.136.101

--- part1/server/routes/api.js
+++ part1/server/routes/api.js
@@ -23,7 +23,7 @@ router.get('/walkrequests/open', async function(req, res) {
     database: 'DogWalkService'
   });
   const [dogs] = await db.execute(`
-    SELECT WalkRequests.request_id, Dogs.name as dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username FROM Dogs JOIN Users ON Dogs.owner_id=Users.user_id JOIN 
+    SELECT WalkRequests.request_id, Dogs.name as dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username FROM Dogs JOIN Users ON Dogs.owner_id=Users.user_id JOIN WalkRequests ON Dogs.dog_id=WalkRequests.dog_id WHERE 
   `);
   res.send(dogs);
 });
