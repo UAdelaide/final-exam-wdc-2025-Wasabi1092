bc65c14591edd366eb164aa21e6b8c2d057a3f83
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 02:54:29 +0000

    [automatic commit] 2025-06-20 02:54:29 UTC 1.158.136.101

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -187,13 +187,13 @@ function login(){
     // Define function to run on response
     xmlhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            if 
+            if
         }
     };
 
     // Open connection to server & send the post data using a POST request
     // We will cover POST requests in more detail in week 8
-    xmlhttp.open("POST", "/users/login", true);
+    xmlhttp.open("POST", "/login", true);
     xmlhttp.setRequestHeader("Content-type", "application/json");
     xmlhttp.send(JSON.stringify(user));
 
