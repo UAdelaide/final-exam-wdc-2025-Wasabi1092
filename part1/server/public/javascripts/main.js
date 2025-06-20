da7517e448f28e21e16812532301691f151e57e3
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 02:04:57 +0000

    [automatic commit] 2025-06-20 02:04:57 UTC 1.158.136.101

--- part1/server/public/javascripts/main.js
+++ part1/server/public/javascripts/main.js
@@ -1,16 +1,9 @@
 import { createApp } from 'vue';
 
-let url = (async() => {
-  let response = await fetch("https://dog.ceo/api/breeds/image/random");
-  let object = await response.json();
-  return object.message;
-})();
-
-console.log(url);
 const app = createApp({
   data() {
     return {
-      image: url
+      image: "hello"
     };
   }
 });
