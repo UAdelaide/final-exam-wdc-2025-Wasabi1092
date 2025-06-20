8af80f330501fe95d947836313274dc435be9d79
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 01:59:54 +0000

    [automatic commit] 2025-06-20 01:59:54 UTC 1.158.136.101

--- part1/server/public/javascripts/main.js
+++ part1/server/public/javascripts/main.js
@@ -4,6 +4,7 @@ const app = createApp({
   async setup() {
     let response = await fetch("https://dog.ceo/api/breeds/image/random");
     let object = await response.json();
+    console.log(object.message);
     return {
       image: object.message
     };
