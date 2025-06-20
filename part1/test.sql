6b787e113da0f09da3e83d0fb834f3f0e0c1aaa9
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 00:34:50 +0000

    [automatic commit] 2025-06-20 00:34:50 UTC 1.158.136.101

--- part1/test.sql
+++ part1/test.sql
@@ -17,4 +17,8 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username='carol123'), 'Monty', 'small');
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, ')
+((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name='Bobby'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
