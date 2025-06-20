059354b72ded4d836f691878f2848379c4f36913
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 01:07:08 +0000

    [automatic commit] 2025-06-20 01:07:08 UTC 1.158.136.101

--- part1/test.sql
+++ part1/test.sql
@@ -26,3 +26,6 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
 
 -- Insert reviews into table WalkRatings;
 INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
+(
+  
+)
