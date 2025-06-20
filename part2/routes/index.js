5897444caf5e658c3c715b82431b003dbbd92147
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 03:02:12 +0000

    [automatic commit] 2025-06-20 03:02:12 UTC 1.158.136.101

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -4,10 +4,10 @@ const db = require('../models/db');
 
 
 router.post("/login", (req, res) => {
-  const [rows] = db.execute("SELECT * FROM Users WHERE username=?", [user]);
+  const [rows] = db.execute("SELECT * FROM Users WHERE username=?", [req.body.user]);
   if (rows.length === 0) {
     res.status(200).json({ result: 'failure' });
-  } else if (rows[0].password_hash === pass) {
+  } else if (rows[0].password_hash === req.body.pass) {
     res.status(200).json({ result: 'success', user_type: rows[0].role });
   } else {
     res.status(200).json({ result: 'failure' });
