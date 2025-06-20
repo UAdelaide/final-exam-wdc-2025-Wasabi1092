91120e1612ab2b2dd031233b96b92d57b059db93
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 01:36:02 +0000

    [automatic commit] 2025-06-20 01:36:02 UTC 1.158.136.101

--- part1/server/routes/api.js
+++ part1/server/routes/api.js
@@ -51,6 +51,17 @@ router.get('/walkers/summary', async function (req, res) {
       rating_sum: 0
     });
   }
+  const [data] = await db.execute(`
+    SELECT
+    Users.username AS walker_username,
+    COUNT(WalkRatings.walker_id) AS total_ratings,
+    AVG(WalkRatings.rating) AS average_rating,
+    COUNT(CASE WHEN WalkRequests.status='completed' THEN 1 END) AS completed_walks
+    FROM Users
+    JOIN WalkRatings ON WalkRatings.walker_id=Users.user_id
+    JOIN WalkRequests ON WalkRequests.request_id=WalkRatings.request_id
+    GROUP BY Users.username;
+  `);
   
   res.send(walkers);
 });
