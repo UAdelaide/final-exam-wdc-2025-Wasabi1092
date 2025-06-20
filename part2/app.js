d82226c51cd87d952db590d5c98abfe91ce0064f
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 02:59:11 +0000

    [automatic commit] 2025-06-20 02:59:11 UTC 1.158.136.101

--- part2/app.js
+++ part2/app.js
@@ -17,17 +17,7 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-app.use('/', router.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  const [rows] = db.execute("SELECT * FROM Users WHERE username=?", [username]);
-  if (rows.length === 0) {
-    res.status(200).json({ result: 'failure' });
-  } else if (rows[0].password_hash === password) {
-    res.status(200).json({ result: 'success', user_type: rows[0].role });
-  } else {
-    res.status(200).json({ result: 'failure' });
-  }
-}));
+app.use('/', indexRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
---
+++ part2/routes/index.js
@@ -0,0 +1,13 @@
+
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const [rows] = db.execute("SELECT * FROM Users WHERE username=?", [username]);
+  if (rows.length === 0) {
+    res.status(200).json({ result: 'failure' });
+  } else if (rows[0].password_hash === password) {
+    res.status(200).json({ result: 'success', user_type: rows[0].role });
+  } else {
+    res.status(200).json({ result: 'failure' });
+  }
+})
