028cb65a95aa216dbc519b2d688475f02d172710
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 02:35:18 +0000

    [automatic commit] 2025-06-20 02:35:18 UTC 1.158.136.101

--- part2/app.js
+++ part2/app.js
@@ -16,9 +16,9 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-app.use('/login', function() {
-
-})
+app.use('/', router.get("/login", (req, res) {
+  
+}))
 
 // Export the app instead of listening here
 module.exports = app;
