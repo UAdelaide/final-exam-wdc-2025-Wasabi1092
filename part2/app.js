7358090487c07b03ca0e84b57ef78d082032da3e
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 02:59:51 +0000

    [automatic commit] 2025-06-20 02:59:51 UTC 1.158.136.101

--- part2/app.js
+++ part2/app.js
@@ -13,10 +13,9 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-
+const indexRoutes = require('./routes/index');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-
 app.use('/', indexRoutes);
 
 // Export the app instead of listening here
