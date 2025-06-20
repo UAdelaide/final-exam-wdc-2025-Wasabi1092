c87d5c76dbd28b7815ff49c0456b27111a381b40
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 00:50:59 +0000

    [automatic commit] 2025-06-20 00:50:59 UTC 1.158.136.101

--- part1/server/routes/api.js
+++ part1/server/routes/api.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var mysql2 = require("mysql2/promise");
+
 
 /* GET home page. */
-router.get('/dogs', function(req, res) {
+router.get('/dogs', async function(req, res) {
 });
 
 module.exports = router;
