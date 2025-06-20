8bbdaf22c03f46d5af231b3db08f042e1f2e9f40
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 02:36:38 +0000

    [automatic commit] 2025-06-20 02:36:38 UTC 1.158.136.101

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const db = require ('./models/db')
+const db = require ('./models/db');
 require('dotenv').config();
 
 const app = express();
