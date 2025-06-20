4d28bf370af9894140854baa6afbd9579455c782
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 00:31:30 +0000

    [automatic commit] 2025-06-20 00:31:30 UTC 1.158.136.101

--- part1/test.sql
+++ part1/test.sql
@@ -10,4 +10,8 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 
 -- Insert dogs into table Dogs;
 INSERT INTO Dogs (owner_id, name, size) VALUES
-((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium');
+((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username='alice123'), 'Bella', 'medium'),
+((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
