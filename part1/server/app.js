d0066eab43756cd8aad54750ce7387a317bb7432
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 00:41:13 +0000

    [automatic commit] 2025-06-20 00:41:13 UTC 1.158.136.101

--- part1/server/app.js
+++ part1/server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mysql2 = require('mysql2');
 
 var apiRouter = require('./routes/api');
 
@@ -12,6 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+(async () => {
+
+}
 
 app.use('/api', apiRouter);
 
