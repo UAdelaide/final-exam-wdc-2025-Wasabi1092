9e935ccc8d0a7c40a315c04b4855604a06e6de32
Wasabi1092 <xavier.tang109@gmail.com>
2025-06-20 00:48:58 +0000

    [automatic commit] 2025-06-20 00:48:58 UTC 1.158.136.101

--- part1/server/app.js
+++ part1/server/app.js
@@ -95,12 +95,12 @@ app.use(express.static(path.join(__dirname, 'public')));
     [rows] = await db.execute("SELECT COUNT(*) as count from Dogs");
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO Users (username, email, password_hash, role) VALUES
-        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-        ('davidwalker', 'david@example.com', 'hashed012', 'walker'),
-        ('ethan123', 'ethan@example.com', 'hashed345', 'owner');
+        INSERT INTO Dogs (owner_id, name, size) VALUES
+        ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
+        ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
+        ((SELECT user_id FROM Users WHERE username='ethan123'), 'Bobby', 'large'),
+        ((SELECT user_id FROM Users WHERE username='alice123'), 'Richie', 'medium'),
+        ((SELECT user_id FROM Users WHERE username='carol123'), 'Monty', 'small');
       `);
     }
 
